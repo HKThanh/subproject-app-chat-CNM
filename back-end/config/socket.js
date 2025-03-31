d61e955a53c492bf1866741909beb5702e456ee0
HKThanh <21086351.thanh@student.iuh.edu.vn>
2025-03-31 15:19:30 +0700

    add config for socket.io

---
+++ back-end/config/socket.js
@@ -0,0 +1,12 @@
+const { Server } = require("socket.io");
+
+const initSocketIO = server => {
+    const io = new Server(server, {
+        cors: {
+            origin: "*",
+        },
+        maxHttpBufferSize: 1e8,
+    });
+
+    return io;
+}
