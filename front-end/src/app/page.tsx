31d937fa110db24dcd7529ae2c1a9d4d8ca92514
Nguyen Duc Thinh <thinhdz1500@gmail.com>
2025-04-04 11:15:11 +0700

    feat: add login page

---
+++ front-end/src/app/page.tsx
@@ -0,0 +1,49 @@
+import { ChevronRight } from "lucide-react";
+import Image from "next/image";
+import RotatingText from "../../component/react-bits/text-animations/RotatingText";
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <div className="flex items-center gap-2">
+          <span className="text-2xl sm:text-3xl md:text-4xl font-bold bg-gradient-to-r from-blue-600 to-cyan-500 text-transparent bg-clip-text">
+            WeChat
+          </span>
+          <RotatingText
+            texts={[
+              "Kết nối mọi lúc",
+              "Chat nhóm tiện lợi",
+              "Chia sẻ tức thì",
+              "Luôn bên bạn"
+            ]}
+            mainClassName="px-4 sm:px-6 md:px-8 bg-gradient-to-r from-blue-500 to-cyan-400 text-white overflow-hidden py-2 sm:py-3 md:py-4 justify-center rounded-xl font-bold text-lg sm:text-xl md:text-2xl"
+            staggerFrom={"first"}
+            initial={{ y: "100%", opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            exit={{ y: "-100%", opacity: 0 }}
+            staggerDuration={0.03}
+            splitLevelClassName="overflow-hidden pb-1 sm:pb-2"
+            transition={{ 
+              type: "spring", 
+              damping: 20, 
+              stiffness: 300,
+              bounce: 0.3
+            }}
+            rotationInterval={3000}
+            splitBy="words"
+          />
+        </div>
+        <div className="flex gap-4 items-center flex-col sm:flex-row ">
+          <a
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+            href="/auth/login"
+          >
+            chat now
+            <ChevronRight />
+          </a>
+        </div>
+      </main>
+    </div>
+  );
+}
